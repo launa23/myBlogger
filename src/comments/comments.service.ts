c96d13a1d08245aaf7784adafd0bd23c0e96f7a4
Laun <luan2003hhbg@gmail.com>
2024-06-25 17:09:57 +0700

    Get comment by post

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,12 +1,12 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
-import { createMongooseAsyncProviders } from "@nestjs/mongoose/dist/mongoose.providers";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { Comment } from "./entities/comment.entity";
 import { UsersService } from "../users/users.service";
 import { PostsService } from "../posts/posts.service";
+import { buildCommentTree } from "../utils/transform";
 
 @Injectable()
 export class CommentsService {
@@ -23,16 +23,15 @@ export class CommentsService {
       this.postService.findOne(createCommentDto.postId),
       createCommentDto.parentId ? this.findOne(createCommentDto.parentId, "Không tìm thấy comment mà bạn muốn trả lời ") : null,
     ]);
-
-    const commentData = {
-      content: createCommentDto.content,
-      user,
-      parent: parentComment,
-      post,
-    };
-
+    if ( parentComment && parentComment.post.id !== createCommentDto.postId ) {
+      throw new BadRequestException("Không thể bình luận! ");
+    }
+    const commentData = new Comment();
+    commentData.content = createCommentDto.content;
+    commentData.user = user;
+    commentData.parent = parentComment;
+    commentData.post = post;
     const comment = await this.commentRepository.save(commentData);
-
     return {
       id: comment.id,
       content: comment.content,
@@ -42,9 +41,22 @@ export class CommentsService {
     };
   }
 
-  findAll() {
-    return `This action returns all comments`;
+  async findAllByPost(id: number){
+    const qb = await this.commentRepository.createQueryBuilder('cmt')
+      .leftJoinAndSelect('cmt.parent', 'parent')
+      .leftJoinAndSelect('cmt.post', 'post')
+      .leftJoinAndSelect('cmt.user', 'user')
+      .where("post.id = :id", {id})
+      .andWhere('cmt.isDeleted is false')
+      .select(['cmt.id', 'parent.id', 'cmt.content', 'post.title', 'post.id', 'user.id', 'user.name'])
+      .getRawMany();
+    return {
+      post_id: id,
+      total: qb.length,
+      comments: buildCommentTree(qb)
+    };
   }
+
 
   async findOne(id: number, message: string) {
     const comment = await this.commentRepository.findOneBy({id: id});
@@ -55,7 +67,7 @@ export class CommentsService {
   }
 
   async update(id: number, updateCommentDto: UpdateCommentDto) {
-    let post = await this.postService.findOne(updateCommentDto.postId);
+    const post = await this.postService.findOne(updateCommentDto.postId);
     await this.findOne(id, `Không tìm thấy comment với id là ${id}`);
     return await this.commentRepository.update({id: id},{content: updateCommentDto.content})
   }
@@ -65,3 +77,6 @@ export class CommentsService {
     return await this.commentRepository.update({id: id},{isDeleted: true, deletedAt: new Date()})
   }
 }
+
+
+
